433cfd48325fd4bdde624a8acd2ea13006bc658c
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2022-11-22 23:40:27 -0500

    First commit blank spring boot project

---
+++ demoV1/src/test/java/com/am/demov1/DemoV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.am.demov1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

dca181c5003314d335a478326602c9f001661ec5
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2022-11-24 23:40:11 -0500

    Day 3 commit blank spring boot project without DB, but with a Get API function

--- demo2/src/main/java/com/am/demo2/Demo2Application.java
+++ demo2/src/main/java/com/am/demo2/Demo2Application.java
@@ -2,12 +2,21 @@ package com.am.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Demo2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo2Application.class, args);
+    }
+
+    //@GetMapping("/hello")  // http://localhost:8080/hello
+    @GetMapping  // http://localhost:8080
+    public String hello() {
+        return "Hello World!";
     }
 
 }
